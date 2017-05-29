5616d101cd7d8138f0bf995f64056e9cfb8fce21
Tung Pham <phamthanhtung1995@gmail.com>
2017-05-29 17:45:33 +0700

    view photo (onProgressing)

--- app/src/main/java/com/app/ptt/comnha/Activity/ViewPhotoActivity.java
+++ app/src/main/java/com/app/ptt/comnha/Activity/ViewPhotoActivity.java
@@ -2,20 +2,46 @@ package com.app.ptt.comnha.Activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
+import com.app.ptt.comnha.Adapters.ViewPhotoVPadapter;
+import com.app.ptt.comnha.Classes.ZoomOutPageTransformer;
+import com.app.ptt.comnha.Models.FireBase.Image;
 import com.app.ptt.comnha.R;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
 
 /**
  * Created by PTT on 4/21/2017.
  */
 
 public class ViewPhotoActivity extends AppCompatActivity {
+    ViewPager vp_viewphoto;
+    ArrayList<Image> images;
+    StorageReference stRef;
+    ViewPhotoVPadapter photoAdapter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_viewphoto);
+        stRef = FirebaseStorage.getInstance().getReferenceFromUrl(getString(R.string.firebaseStorage_path));
+        init();
+    }
+
+    private void init() {
+        vp_viewphoto = (ViewPager) findViewById(R.id.viewpager_viewphoto);
+        images = new ArrayList<>();
+        photoAdapter = new ViewPhotoVPadapter(images, this, stRef);
+        vp_viewphoto.setAdapter(photoAdapter);
+        vp_viewphoto.setPageTransformer(true, new ZoomOutPageTransformer());
     }
 }
---
+++ app/src/main/java/com/app/ptt/comnha/Adapters/ViewPhotoVPadapter.java
@@ -0,0 +1,66 @@
+package com.app.ptt.comnha.Adapters;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.app.ptt.comnha.Models.FireBase.Image;
+import com.app.ptt.comnha.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by PTT on 11/28/2016.
+ */
+
+public class ViewPhotoVPadapter extends PagerAdapter {
+    ArrayList<Image> images;
+    Activity activity;
+    StorageReference stRef;
+
+    public ViewPhotoVPadapter(ArrayList<Image> images, Activity activity, StorageReference stRef) {
+        this.images = images;
+        this.activity = activity;
+        this.stRef = stRef;
+    }
+
+    @Override
+    public int getCount() {
+        return images.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ViewGroup layout = (ViewGroup) LayoutInflater.from(activity)
+                .inflate(R.layout.layout_pager_viewphoto, container, false);
+        container.addView(layout);
+        final ImageView imgv = (ImageView) layout.findViewById(R.id.imgv_viewphoto);
+        StorageReference imgRef = stRef.child(images.get(position).getName());
+        imgRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.with(activity)
+                        .load(uri)
+                        .into(imgv);
+            }
+        });
+        return layout;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object view) {
+        container.removeView((View) view);
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+}
