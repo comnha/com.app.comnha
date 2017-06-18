55b6a3cccf8443aa3c1bd7647b19b8daf8d25f22
ciqaz <cuongdollar2995@gmail.com>
2017-06-18 22:54:47 +0700

    Develop (#3)
    * [CuongNV] update map, gps
    
    * gradle
    
    * [CuongNV] update map, gps

--- app/src/main/java/com/app/ptt/comnha/Activity/BaseActivity.java
+++ app/src/main/java/com/app/ptt/comnha/Activity/BaseActivity.java
@@ -177,6 +177,7 @@ public class BaseActivity extends AppCompatActivity {
             case MULTIPLE_PERMISSIONS: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
+
                 } else {
 
                 }
